f8aba8bb795afef40f184056cebe719ba0a5d49b
Pradeep <pradeepachar12@gmail.com>
2021-11-18 20:51:20 +0530

    api for login added

---
+++ SportStoreApi/Controllers/AuthenticationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SportStoreApi.Models;
+
+namespace SportStoreApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly StoreDbContext _context;
+
+        public AuthenticationController(StoreDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] Customer customer)
+        {
+            var user = _context.Customers.Where(u => u.Email == customer.Email).FirstOrDefault();
+            if(user == null)
+            {
+                return Ok(user);
+            }
+            return Ok(user);
+        }
+
+    }
+}
